01d610b51d37076ff7fb35e9051e7528a1c8915b
Kevin8637 <kevin.lar@live.fr>
2025-01-09 16:59:06 +0100

    Added navbar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import LayoutWithBars from "./src/component/layout/LayoutWithBars";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <LayoutWithBars/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/component/navbar/SearchBar.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import {View, Text} from "react-native";
+
+const MyComponent: FC<{}> = ({}) => {
+    return (
+        <View>
+            <Text>Bla</Text>
+        </View>
+    );
+};
+
+export default MyComponent;
